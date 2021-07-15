dbca7f16762e5710e170de81a87796fb15b07dc1
rory6103 <rory6103@gmail.com>
2021-07-14 22:01:05 -0700

    selecting multiple columns

--- aggregation.py
+++ aggregation.py
@@ -1,19 +1,25 @@
 import numpy as np
 import pandas as pd
+import itertools as it
 
 CITYID_PATH = './city.csv'
 LAND_CITY = './land_city.csv'
 SAVE_PATH = 'aggregated.csv'
 
-def aggregate(dataframe, col='total', types=[]):
+def aggregate(dataframe, col=['total'], types=[]):
     city_year_unique = list(dataframe.index.unique())
     if types == []:
-        if col=='total':
+        if col==['total']:
             types = ['total']
+        elif len(col) == 1:
+            types = list(dataframe[col[0]].unique())
         else:
-            types = list(dataframe[col].unique())
-    arr = np.zeros((len(city_year_unique), 3*len(types)))
-    if col=='total':
+            lotypes = [list(dataframe[column].unique()) for column in col]
+            types = [list(ele) for ele in it.product(*lotypes)]
+            print(types)
+
+    arr = np.zeros((len(city_year_unique), 3*len(types))) if len(col) == 1 else np.zeros((len(city_year_unique), 3*np.prod([len(type) for type in types])))
+    if col==['total']:
         for i, cyear in enumerate(city_year_unique):
             # Extracting multiple rows with same index using loc
             df_temp = dataframe.loc[cyear]
@@ -29,6 +35,23 @@ def aggregate(dataframe, col='total', types=[]):
                 arr[i][2] = np.sum(areas)
             else:
                 print("unexpected")
+    elif len(col) == 1:
+        col = col[0]
+        for i, cyear in enumerate(city_year_unique):
+            # Extracting multiple rows with same index using loc
+            df_temp = dataframe.loc[cyear]
+            for j, type in enumerate(types):
+                if len(df_temp.shape) == 1 and df_temp[col]==type:
+                    arr[i][j*3] = 1
+                    arr[i][j*3+1] = df_temp['deal_price_million']
+                    arr[i][j*3+2] = df_temp['area_ha']
+                elif len(df_temp.shape) == 2:
+                    type_df = df_temp[df_temp[col]==type]
+                    arr[i][j*3] = len(type_df)
+                    prices = np.array(type_df['deal_price_million'])
+                    arr[i][j*3+1] = np.sum(prices)
+                    areas = np.array(type_df['area_ha'])
+                    arr[i][j*3+2] = np.sum(areas)
     else:
         for i, cyear in enumerate(city_year_unique):
             # Extracting multiple rows with same index using loc
@@ -57,17 +80,22 @@ df = pd.read_csv(LAND_CITY, index_col='city_year', low_memory=False)
 df.fillna(0 ,inplace=True)
 cols = list(df.columns)
 
-# Retrieve the IDs associated with each city
+### Retrieve the IDs associated with each city
 city_id_df = pd.read_csv(CITYID_PATH, low_memory=False)
 city_labels = list(city_id_df['city.name'])
 city_ids = list(city_id_df['city.id'])
-# Construct a dictionary that maps the IDs of the cities to their names
+### Construct a dictionary that maps the IDs of the cities to their names
 city_ids = dict(zip(city_ids, city_labels))
 total_temp = aggregate(df)
 cities = [city_ids[city_year//100] for city_year in total_temp.index]
 total_temp.insert(0, "cityID", cities)
-forway_temp = aggregate(df, col='forway')
-land_source_temp = aggregate(df, col='land_source')
-frames = [total_temp, forway_temp, land_source_temp]
-result = pd.concat(frames, axis=1, join="inner")
-result.to_csv(SAVE_PATH)
+
+### individual forway and land source
+# forway_temp = aggregate(df, col=['forway'])
+# land_source_temp = aggregate(df, col=['land_source'])
+# frames = [total_temp, forway_temp, land_source_temp]
+
+forway_land_source_temp = aggregate(df, col=['forway', 'land_source'])
+
+# result = pd.concat(frames, axis=1, join="inner")
+# result.to_csv(SAVE_PATH)
