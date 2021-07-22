221129e361cfe2e2817a19237d59fddfaa6c0eec
rory6103 <rory6103@gmail.com>
2021-07-21 18:03:27 -0700

    skip inserting industry and sector

--- draft.py
+++ draft.py
@@ -62,9 +62,9 @@ df.fillna(0, inplace=True)
 labels = list(df['company_industry'])
 sub_industry = [l if l in CATEGORY_MAP else INDUSTRY_MAP[l] for l in labels]
 industry = [CATEGORY_MAP[s] for s in sub_industry]
-df.insert(8, 'industry', industry)
+# df.insert(8, 'industry', industry)
 sector = [SECTOR_MAP[i] for i in industry]
-df.insert(9, 'sector', sector)
+# df.insert(9, 'sector', sector)
 count = Counter(sector)
 print(count)
 sectors1 = map(lambda x: SECTOR_MAP[CATEGORY_MAP[x]], labels)
